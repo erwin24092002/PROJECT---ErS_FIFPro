be3f4e9c56b653f9fc73a0dd2cf71c8d62b8a7a5
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2023-01-06 16:48:03 +0700

    update lots

--- ErS FIFPro/Forms/fMatch.Designer.cs
+++ ErS FIFPro/Forms/fMatch.Designer.cs
@@ -44,6 +44,14 @@
             this.pTeam13 = new System.Windows.Forms.Panel();
             this.pTeam14 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -192,6 +200,70 @@
             this.pictureBox1.TabIndex = 30;
             this.pictureBox1.TabStop = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(128, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 5);
+            this.panel1.TabIndex = 31;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DimGray;
+            this.panel2.Location = new System.Drawing.Point(779, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 5);
+            this.panel2.TabIndex = 32;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DimGray;
+            this.panel3.Location = new System.Drawing.Point(128, 657);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 5);
+            this.panel3.TabIndex = 33;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DimGray;
+            this.panel4.Location = new System.Drawing.Point(779, 657);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(400, 5);
+            this.panel4.TabIndex = 34;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.DimGray;
+            this.panel5.Location = new System.Drawing.Point(313, 41);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(5, 630);
+            this.panel5.TabIndex = 35;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.DimGray;
+            this.panel6.Location = new System.Drawing.Point(966, 41);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(5, 630);
+            this.panel6.TabIndex = 36;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.DimGray;
+            this.panel7.Location = new System.Drawing.Point(349, 352);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(600, 5);
+            this.panel7.TabIndex = 37;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.DimGray;
+            this.panel8.Location = new System.Drawing.Point(641, 267);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(5, 100);
+            this.panel8.TabIndex = 38;
+            // 
             // fMatch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -213,6 +285,14 @@
             this.Controls.Add(this.pTeam5);
             this.Controls.Add(this.pTeam2);
             this.Controls.Add(this.pTeam1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel8);
             this.Name = "fMatch";
             this.Text = "fMatch";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -238,5 +318,13 @@
         private System.Windows.Forms.Panel pTeam13;
         private System.Windows.Forms.Panel pTeam14;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
     }
 }
